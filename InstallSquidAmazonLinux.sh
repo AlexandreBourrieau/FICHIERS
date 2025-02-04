fb0dfbf4314da53c553b591ebfd635b36065dc87
AlexandreBourrieau <66630728+AlexandreBourrieau@users.noreply.github.com>
2025-02-04 14:16:59 +0100

    Update InstallSquidAmazonLinux.sh

--- InstallSquidAmazonLinux.sh
+++ InstallSquidAmazonLinux.sh
@@ -4,5 +4,4 @@ yum update -y
 yum install -y squid
 systemctl enable squid
 sed -i 's/http_access deny all/http_access allow all/' /etc/squid/squid.conf
-echo 'acl allow_my_ip src {YOUR_IP}/32' | sudo tee -a /etc/squid/squid.conf
 systemctl start squid
