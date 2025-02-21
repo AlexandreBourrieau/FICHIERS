49ee9af31287e791cebae3a3fa8f16b2e2cef0f4
AlexandreBourrieau <66630728+AlexandreBourrieau@users.noreply.github.com>
2025-02-21 15:23:00 +0100

    Update InstallSquidAmazonLinux.sh

--- InstallSquidAmazonLinux.sh
+++ InstallSquidAmazonLinux.sh
@@ -5,6 +5,6 @@ yum install -y squid
 yum install -y httpd-tools
 htpasswd -b -c /etc/squid/passwords proxyuser proxypass
 systemctl enable squid
-sed -i '3,1s/^/auth_param basic program \/usr\/lib64\/squid\/basic_ncsa_auth \/etc\/squid\/passwords\nauth_param basic realm proxy\nacl authenticated proxy_auth REQUIRED\ \n \n/' /etc/squid/squid.conf
+sed -i '4,1s/^/auth_param basic program \/usr\/lib64\/squid\/basic_ncsa_auth \/etc\/squid\/passwords\nauth_param basic realm proxy\nacl authenticated proxy_auth REQUIRED\ \n \n/' /etc/squid/squid.conf
 sed -i 's/http_access deny all/http_access allow authenticated\nhttp_access deny all/' /etc/squid/squid.conf
 systemctl start squid
